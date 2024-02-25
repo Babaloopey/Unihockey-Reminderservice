b756d37f622492896a5b673e2ca432fa7b81e61c
Rood <48691039+Babaloopey@users.noreply.github.com>
2024-02-25 18:27:50 +0100

    feat: Initialize project

---
+++ src/main/java/com/unihockeyreminderservice/UnihockeyReminderserviceApplication.java
@@ -0,0 +1,13 @@
+package com.unihockeyreminderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnihockeyReminderserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnihockeyReminderserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unihockeyreminderservice/UnihockeyReminderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unihockeyreminderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnihockeyReminderserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
